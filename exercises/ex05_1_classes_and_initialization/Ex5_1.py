ca76560dcaba4ce37998b0a62d603e98cb8a62fa
Michal Klimek <klimos@poczta.fm>
2025-03-27 08:44:09 +0100

    solution for exercises 5.1 and 5.2

--- exercises/ex05_1_classes_and_initialization/Ex5_1.py
+++ exercises/ex05_1_classes_and_initialization/Ex5_1.py
@@ -3,8 +3,7 @@ Exercise 5.1 Classes and Initialization
 Ex5_1.py
 """
 
-# Part A
-
+# Classes definition
 class Trip:
 
     cariblist = ['GCM', 'CUR']
@@ -17,38 +16,92 @@ class Trip:
         self.departdaytime = departdaytime
         self.arrivedaytime = arrivedaytime
 
+    def is_round_trip(self):
+        return self.departcity == self.arrivecity
+
+    def is_hawaiian(self):
+        return self.arrivecity in Trip.hawaiilist
+
+    def is_caribbean(self):
+        return self.arrivecity in Trip.cariblist
+
+    def is_interisland(self):
+        return self.departcity in Trip.hawaiilist and self.arrivecity in Trip.hawaiilist
+
+
+class Aircraft:
+    def __init__(self, code=None, name=None):
+        self.code = code
+        self.name = name
+
+
+class Airport:
+    def __init__(self, citycode=None, city=None):
+        self.citycode = citycode
+        self.city = city
+
+
+#Part A
 # Sample data for Trip
 depcity = 'CUR'
 arrcity = 'HNL'
 depdt = '2022-01-03 09:00'
 arrdt = '2022-01-03 16:00'
-# mytrip = Trip(departcity=depcity,
-#               arrivecity=arrcity,
-#               departdaytime=depdt,
-#               arrivedaytime=arrdt)
+mytrip = Trip(departcity=depcity,
+              arrivecity=arrcity,
+              departdaytime=depdt,
+              arrivedaytime=arrdt)
+
+print("\nPart A\n" + '-' * 6)
+print("My Trip is departing from '{0}' on {1} and arrives to '{2}' on {3}.".format(
+    mytrip.departcity, mytrip.departdaytime, mytrip.arrivecity, mytrip.arrivedaytime))
+
+print("Caribbean destinations:", Trip.cariblist)
+print("Hawaii destinations:", Trip.hawaiilist)
+
+print('-' * 6)
+ans = 'is' if mytrip.is_round_trip() else 'is not'
+print("The trip from", mytrip.departcity, "to", mytrip.arrivecity, ans, "a round trip.")
+
 
 # Part B
 # Create list of Trip objects, pass list as positional args to constructor
+alltrips = [Trip(*['HNL', 'HKG', '2022-01-03 16:00', '2022-01-03 20:00']),
+Trip(*['HNL', 'HNL', '2022-01-03 08:30', '2022-01-03 15:40']),
+Trip(*['HKG', 'CDG', '2022-01-03 19:20', '2022-01-04 12:35']),
+Trip(*['HKG', 'GCM', '2022-01-03 16:50', '2022-01-04 09:30']),
+Trip(*['HNL', 'ITO', '2022-01-03 12:00', '2022-01-03 13:55'])]
 
-# alltrips = [Trip(*['HNL', 'HKG', '2022-01-03 16:00', '2022-01-03 20:00']),
-# Trip(*['HNL', 'HNL', '2022-01-03 08:30', '2022-01-03 15:40']),
-# Trip(*['HKG', 'CDG', '2022-01-03 19:20', '2022-01-04 12:35']),
-# Trip(*['HKG', 'GCM', '2022-01-03 16:50', '2022-01-04 09:30']),
-# Trip(*['HNL', 'ITO', '2022-01-03 12:00', '2022-01-03 13:55'])]
 
-#
-# def print_trip(trp):
-#     print('Trip from', trp.departcity, 'to', trp.arrivecity,
-#             'Departs at', trp.departdaytime,
-#             'Arrives at', trp.arrivedaytime, end=' ')
+def print_trip(trp):
+    print('Trip from', trp.departcity, 'to', trp.arrivecity,
+            'Departs at', trp.departdaytime,
+            'and arrives at', trp.arrivedaytime)
 
+
+print("\nPart B\n" + '-' * 6)
+for trip in alltrips:
+    print_trip(trip)
+    if trip.is_round_trip():
+        print("\tis a round trip")
+    if trip.is_caribbean():
+        print("\tis Caribbran")
+    if trip.is_hawaiian():
+        print("\tis Hawaiian")
+    if trip.is_interisland():
+        print("\tis an interisland trip")
 
 
 # Part C
 # Sample Data for Aircraft
-# code is 1
-# name is Canadian Regional Jet
-#
+code = 1
+name = "Canadian Regional Jet"
+myplane = Aircraft(code, name)
+print("\nPart C\n" + '-' * 6)
+print(f"My aircraft is {myplane.name} and has code '{myplane.code}'.")
+
 # Sample Data for Airport
-# code is HNL
-# citycity is Honolulu
+citycode = 'HNL'
+city = "Honolulu"
+myairport = Airport(citycode, city)
+print(f"Airport in {myairport.city} has code '{myairport.citycode}'.")
--- exercises/ex05_2_inheritance/Ex5_2.py
+++ exercises/ex05_2_inheritance/Ex5_2.py
@@ -4,7 +4,7 @@ Ex5_2.py
 """
 
 # Part A
-
+# Base class definition
 class Trip:
 
     cariblist = ['CUR', 'GCM']
@@ -32,11 +32,32 @@ class Trip:
 
 # Part B
 # Add the subclass below
+class Flight(Trip):
 
-class Flight:
+    disc_interisland = 0.05
+    disc_hawaiian = 0.1
+    disc_caribbean = 0.15
+
     def __init__(self, flightnum=-1, cost=0.0, code=0, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.flightnum = flightnum
+        self.cost = cost
+        self.code = code
 
+    def discount(self):
+        if self.is_interisland():
+            self.cost -= self.cost * Flight.disc_interisland
+        elif self.is_hawaiian():
+            self.cost -= self.cost * Flight.disc_hawaiian
+        elif self.is_hawaiian():
+            self.cost -= self.code * Flight.disc_hawaiian
+
+
+class Reservation:
+    def __init__(self, name=None, reservationid=None, flightref=None):
+        self.name = name
+        self.reservationid = reservationid
+        self.flightref = flightref
 
 # Part C
 # Sample data for Flight
@@ -48,25 +69,47 @@ arrcity = 'HNL'
 depdt = '2022-01-03 09:00'
 arrdt = '2022-01-03 16:00'
 
-# mytrip = Flight(flightnum=fnum,
-#                 cost = cost,
-#                 code = craftcode,
-#                 departcity=depcity,
-#                 arrivecity=arrcity,
-#                 departdaytime=depdt,
-#                 arrivedaytime=arrdt)
+mytrip = Flight(flightnum=fnum,
+                cost = cost,
+                code = craftcode,
+                departcity=depcity,
+                arrivecity=arrcity,
+                departdaytime=depdt,
+                arrivedaytime=arrdt)
 
-# allflights = [Flight(*[317, 99.95, 4, 'CUR', 'ITO', '2022-01-03 16:00', '2022-01-03 20:00']),
-#               Flight(*[102, 199.99, 42, 'HNL', 'HNL', '2022-01-03 08:30', '2022-01-03 15:40']),
-#               Flight(*[204, 299.99, 44, 'HKG', 'CDG', '2022-01-03 19:20', '2022-01-04 12:35']),
-#               Flight(*[336, 199.99, 44, 'HKG', 'GCM', '2022-01-03 16:50', '2022-01-04 09:30']),
-#               Flight(*[660, 299.99, 3, 'HNL', 'ITO', '2022-01-03 12:00', '2022-01-03 13:55'])]
+print("My Flight:")
+print(vars(mytrip))
+print("-" * 10)
+
+allflights = [Flight(*[317, 99.95, 4, 'CUR', 'ITO', '2022-01-03 16:00', '2022-01-03 20:00']),
+              Flight(*[102, 199.99, 42, 'HNL', 'HNL', '2022-01-03 08:30', '2022-01-03 15:40']),
+              Flight(*[204, 299.99, 44, 'HKG', 'CDG', '2022-01-03 19:20', '2022-01-04 12:35']),
+              Flight(*[336, 199.99, 44, 'HKG', 'GCM', '2022-01-03 16:50', '2022-01-04 09:30']),
+              Flight(*[660, 299.99, 3, 'HNL', 'ITO', '2022-01-03 12:00', '2022-01-03 13:55'])]
+
+print("All flights:")
+for flight in allflights:
+    print(vars(flight))
+print("-" * 10)
+
+print("All flight prices with discount applied (if available):")
+for flight in allflights:
+    print("Flight", flight.flightnum, "orignal cost:", flight.cost, end=", ")
+    flight.discount()
+    print("after discount:", f"{flight.cost:.2f}")
+print("-" * 10)
+
 
 # Part D
+# Sample data for Reservation
+reservations = [{"name": "Pat Holder", "reservationid": "101A", "flightref": allflights[0]},
+                {"name": "Peter Smith", "reservationid": "102B", "flightref": allflights[1]},
+                {"name": "Guy Gildersleeve", "reservationid": "103C", "flightref": allflights[2]},
+                {"name": "Janet Rider", "reservationid": "104D", "flightref": allflights[4]},
+                {"name": "Lynn Jasper", "reservationid": "105E", "flightref": allflights[2]},
+                {"name": "Ian Rouselle", "reservationid": "106F", "flightref": allflights[0]}]
 
-reservations = [{"name": "Pat Holder", "reservationid": "101A", "flightref": None},
-                {"name": "Peter Smith", "reservationid": "102B", "flightref": None},
-                {"name": "Guy Gildersleeve", "reservationid": "103C", "flightref": None},
-                {"name": "Janet Rider", "reservationid": "104D", "flightref": None},
-                {"name": "Lynn Jasper", "reservationid": "105E", "flightref": None},
-                {"name": "Ian Rouselle", "reservationid": "106F", "flightref": None}]
+for res in reservations:
+    reservation = Reservation(**res)
+    print(f"Reservation no. {reservation.reservationid} - name: {reservation.name},"
+          f" flight: '{reservation.flightref.flightnum}', price: {reservation.flightref.cost:.2f}")
